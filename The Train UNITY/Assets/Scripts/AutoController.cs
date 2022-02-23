4cea56881678b2949851a41e4eb5c9a15659695e
Oscar Wilkinson <Oscar2.Wilkinson@live.uwe.ac.uk>
2022-02-23 19:58:56 +0000

    Auto turret works, Zombie works

--- The Train UNITY/Assets/Scripts/AutoController.cs
+++ The Train UNITY/Assets/Scripts/AutoController.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class AutoController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public TurretScript tur_script;
     void Start()
     {
-        
+        tur_script = this.GetComponent<TurretScript>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        GameObject[] zombies = GameObject.FindGameObjectsWithTag("Zombie");
+        bool zombie_in_range = false;
+        Vector3 closest_zombie_vec = new Vector3(0, 0, 0);
+        float closest_zombie_dist = 100;
+        foreach (GameObject zombie in zombies)
+        {
+            Vector3 distance_vec = zombie.transform.position - this.transform.position;
+            float distance = distance_vec.sqrMagnitude;
+            if (distance < tur_script.fire_range && distance < closest_zombie_dist)
+            {
+                closest_zombie_vec = zombie.transform.position;
+                closest_zombie_dist = distance;
+                zombie_in_range = true;
+            }
+        }
+        if (zombie_in_range)
+        {
+            tur_script.Track(closest_zombie_vec);
+            tur_script.Shoot();
+        }
     }
 }
---
+++ The Train UNITY/Assets/Scripts/ZombieScript.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieScript : MonoBehaviour
+{
+    private GameObject designated_carriage;
+    private bool carriage_in_range = false;
+    public float move_speed;
+    public float attack_speed;
+    public float attack_timer;
+    public float attack_range;
+    public CircleCollider2D attack_range_hitbox;
+    void Start()
+    {
+        attack_range_hitbox.radius = attack_range;
+        GameObject[] carriages = GameObject.FindGameObjectsWithTag("Carriage");
+        float closest_carriage_dist = 100;
+        foreach (GameObject carriage in carriages)
+        {
+            Vector3 distance_vec = carriage.transform.position - this.transform.position;
+            float distance = distance_vec.sqrMagnitude;
+            if (distance < closest_carriage_dist)
+            {
+                designated_carriage = carriage;
+                closest_carriage_dist = distance;
+            }
+        }
+    }
+
+    void Update()
+    {
+        if (carriage_in_range)
+        {
+            Attack();
+        }
+        else
+        {
+            Move();
+        }
+        attack_timer += Time.deltaTime;
+    }
+
+    public void Attack()
+    {
+        if (attack_timer > attack_speed)
+        {
+            //Carriage takes damage
+            attack_timer = 0;
+        }
+    }
+
+    public void Move()
+    {
+        Vector3 move_direction = this.transform.position - designated_carriage.transform.position;
+        move_direction.Normalize();
+        this.transform.position = this.transform.position - move_speed * move_direction * Time.deltaTime;
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Carriage")
+        {
+            carriage_in_range = true;
+        }
+    }
+}
