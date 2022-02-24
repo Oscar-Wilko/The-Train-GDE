2f757b4c5406adbb113e749e757f7333bb1a36af
Oscar Lab <Oscar2.Wilkinson@live.uwe.ac.uk>
2022-02-24 16:36:16 +0000

    Added connectors, updated engine, zombie spawner

---
+++ The Train UNITY/Assets/Scripts/Zombie/ZombieSpawnScript.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieSpawnScript : MonoBehaviour
+{
+    public float spawn_point_y;
+    public float spawn_point_x;
+    public bool allow_vertical_spawns;
+    public float spawn_frequency;
+    public float spawn_timer = 0;
+    public int wave_counter = 0;
+    public int max_waves;
+    public int zombies_per_wave;
+    public int zombies_spawned_this_wave = 0;
+    public GameObject zombie_prefab;
+
+    void Update()
+    {
+        spawn_timer += Time.deltaTime;
+        if (spawn_timer >= (1/spawn_frequency) && Random.Range(0,10) >= 8 && zombies_spawned_this_wave < zombies_per_wave)
+        {
+            Spawn();
+        }
+    }
+
+    public void Spawn()
+    {
+        zombies_spawned_this_wave ++;
+        spawn_timer = 0;
+        float temp_x;
+        float temp_y = Random.Range(-spawn_point_x, spawn_point_x);
+        if (allow_vertical_spawns && Random.Range(0,2) == 0)
+        {
+            temp_x = Random.Range(-spawn_point_x, spawn_point_x);
+            temp_y = spawn_point_y * (Random.Range(0, 2) * 2 - 1);
+        }
+        else
+        {
+            temp_x = spawn_point_x * (Random.Range(0, 2) * 2 - 1);
+            temp_y = Random.Range(-spawn_point_y, spawn_point_y);
+        }
+        Vector3 spawn_pos = new Vector3(temp_x, temp_y, 0);
+        GameObject zombie_instance = Instantiate(zombie_prefab, spawn_pos, Quaternion.identity);
+    }
+}
