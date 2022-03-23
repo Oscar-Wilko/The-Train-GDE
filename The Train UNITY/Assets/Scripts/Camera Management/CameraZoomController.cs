34b05e18fcc5af52d12457e288746fc8baee44d4
Oscar Lab <oscar2.Wilkinson@live.uwe.ac.uk>
2022-03-23 13:54:04 +0000

    Zoom revert nice

--- The Train UNITY/Assets/Scripts/Camera Management/CameraZoomController.cs
+++ The Train UNITY/Assets/Scripts/Camera Management/CameraZoomController.cs
@@ -6,43 +6,19 @@ using UnityEngine.UI;
 public class CameraZoomController : MonoBehaviour
 {
     private Camera cam;
+    private Slider slider;
     public float max_zoom;
     public float min_zoom;
-    public float cur_zoom;
 
     void Start()
     {
         cam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
-        cur_zoom = (cam.orthographicSize - min_zoom) / (max_zoom - min_zoom);
+        slider = this.GetComponent<Slider>();
+        slider.value = (cam.orthographicSize - min_zoom) / (max_zoom - min_zoom);
     }
 
     void Update()
     {
-        float scroll = Input.GetAxis("Mouse ScrollWheel");
-        scroll = scroll * 0.1f;
-        Debug.Log(scroll);
-        if (scroll < 0)
-        {
-            if (cur_zoom >= 1 + scroll)
-            {
-                cur_zoom = 1;
-            }
-            else
-            {
-                cur_zoom += scroll;
-            }
-        } 
-        else 
-        {
-            if (cur_zoom <= scroll)
-            {
-                cur_zoom = 0;
-            }
-            else
-            {
-                cur_zoom += scroll;
-            }
-        }
-        cam.orthographicSize = min_zoom + (max_zoom - min_zoom) * cur_zoom;
+        cam.orthographicSize = min_zoom + (max_zoom - min_zoom) * slider.value;
     }
 }
